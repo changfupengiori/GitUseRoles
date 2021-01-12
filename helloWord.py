117bf5f779af20797860247c82b388b8851b2ae1
changfupeng <changfupengiori@126.com>
2021-01-12 19:36:25 +0800

    fix:B再次修改和A造成冲突

--- helloWord.py
+++ helloWord.py
@@ -8,7 +8,7 @@ class BaseClass():
     def __init__(self):
         pass
     def show(self,a='默认值参数'):
-        print("我是第一个开发人员编写的方法")
+        print("我是第一个开发人员编写的方法"+"B在此修改造成冲突")
         #修复补丁的时候修改了这个位置同时修改了参数
         print("第三个人在此修复bug。同传参："+a)
 
